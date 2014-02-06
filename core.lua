e2de8ef0acb56b1684b56e1c21c7b01073d24e51
Jamie Schembri <jamie@schembri.me>
2014-02-06 22:19:52 +0100

    Handle unknown locales, better structure.

--- core.lua
+++ core.lua
@@ -1,20 +1,35 @@
-local L = {
-  enUS = "Fishing Bobber",
-  deDE = "Angelschwimmer",
-  esES = "Corcho de pesca",
-  frFR = "Flotteur",
-  ruRU = "поплавок",
-  ptBR = "Flutador para Pesca"
-}
+local frame = CreateFrame("Frame")
 
-GameTooltip:HookScript("OnShow", function()
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:SetScript("OnEvent", function()
+  local L = {
+    deDE = "Angelschwimmer",
+    enUS = "Fishing Bobber",
+    esES = "Corcho de pesca",
+    frFR = "Flotteur",
+    ptBR = "Flutador para Pesca",
+    ruRU = "поплавок"
+
+    -- itIT = "",
+    -- esMX = "",
+    -- koKR = "",
+    -- zhCN = "",
+    -- zhTW = "",
+  }
   local localized = L[GetLocale()]
 
-  -- don't do anything if we don't have a string for this locale
-  if not localized then return end
-
-  local tooltipText = GameTooltipTextLeft1
-  if tooltipText and tooltipText:GetText() == localized then
-    GameTooltip:Hide()
+  if not localized then
+    print("|cFFFF0000HideFishingBobberTooltip does not work under your locale. " ..
+          "You can help to fix this by leaving a comment on the Curse or " ..
+          "WoWInterface addon page with the name of the fishing bobber in your language.")
+    return
   end
+  
+  GameTooltip:HookScript("OnShow", function()
+    local tooltipText = GameTooltipTextLeft1
+    if tooltipText and tooltipText:GetText() == localized then
+      GameTooltip:Hide()
+    end
+  end)
 end)
+
